e79a23cbf66d094315d80400d8129a787018b07a
jcasrui733 <jcasrui733@fp.iesromerovargas.com>
2026-06-10 12:45:39 +0200

    cambio comprobacion

--- fuentes/miapp.py
+++ fuentes/miapp.py
@@ -14,7 +14,7 @@ engine = create_engine(DATABASE_URL)
 
 @app.get("/status")
 def status():
-    return {"message": "FastAPI Castro Ruiz-Antonio v.x.y"}
+    return {"message": "FastAPI Castro Ruiz-Antonio v.1.0"}
 
 
 @app.get("/check")
